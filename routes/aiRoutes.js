8d757736173d55cea36aeab1e75a4d057d7bb52b
Caua Gama Paixao <cgpaixao@stefanini.com>
2026-03-25 14:26:30 -0300

    redefinição de fluxo

--- routes/aiRoutes.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const aiController = require('../controllers/aiController');
-
-const router = express.Router();
-
-router.post('/generate-subtasks', aiController.generateSubtasks);
-
-module.exports = router;
